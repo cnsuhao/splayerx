a755db6edb56de5c60fd6904a01547f10e93018d
Tomasen <Tomasen@localhost>
2011-02-11 22:14:10 +0800

    able to verifying app store receipt

--- MPlayerX/ssclThread.m
+++ MPlayerX/ssclThread.m
@@ -110,4 +110,109 @@
 
 }
 
++(CFDataRef) genAppstoreGuid
+{
+  kern_return_t             kernResult;
+  mach_port_t               master_port;
+  CFMutableDictionaryRef    matchingDict;
+  io_iterator_t             iterator;
+  io_object_t               service;
+  CFDataRef                 macAddress = nil;
+  
+  kernResult = IOMasterPort(MACH_PORT_NULL, &master_port);
+  if (kernResult != KERN_SUCCESS) {
+    MPLog(@"IOMasterPort returned %d\n", kernResult);
+    return nil;
+  }
+  
+  matchingDict = IOBSDNameMatching(master_port, 0, "en0");
+  if (!matchingDict) {
+    MPLog(@"IOBSDNameMatching returned empty dictionary\n");
+    return nil;
+  }
+  
+  kernResult = IOServiceGetMatchingServices(master_port, matchingDict, &iterator);
+  if (kernResult != KERN_SUCCESS) {
+    MPLog(@"IOServiceGetMatchingServices returned %d\n", kernResult);
+    return nil;
+  }
+  
+  while((service = IOIteratorNext(iterator)) != 0) {
+    io_object_t parentService;
+    kernResult = IORegistryEntryGetParentEntry(service, kIOServicePlane, &parentService);
+    if (kernResult == KERN_SUCCESS) {
+      if (macAddress) CFRelease(macAddress);
+      macAddress = (CFDataRef) IORegistryEntryCreateCFProperty(parentService,
+                                                               CFSTR("IOMACAddress"), kCFAllocatorDefault, 0);
+      IOObjectRelease(parentService);
+    } else
+      MPLog(@"IORegistryEntryGetParentEntry returned %d\n", kernResult);
+                                               
+    IOObjectRelease(iterator);
+    IOObjectRelease(service);
+  }
+  return macAddress;
+}
+
++(void)authAppstore {
+  
+  NSAutoreleasePool* POOL = [[NSAutoreleasePool alloc] init];	
+	
+  NSString *resPath = [[NSBundle mainBundle] resourcePath];
+	NSString *receiptPath = [resPath stringByAppendingPathComponent:@"_MASReceipt/receipt"];
+  NSFileManager *fileManager = [NSFileManager defaultManager];
+  if ([fileManager fileExistsAtPath:receiptPath] == NO)
+    return [POOL release];
+  
+  
+	NSTask *task;
+	task = [[NSTask alloc] init];
+	[task setLaunchPath: [resPath stringByAppendingPathComponent:@"binaries/x86_64/sscl"] ];
+  NSArray *arguments;
+	arguments = [NSArray arrayWithObjects: @"--uuid", nil];
+	[task setArguments: arguments];	
+	NSPipe *pipe = [NSPipe pipe];
+	[task setStandardOutput: pipe];	
+	NSFileHandle *file;
+	file = [pipe fileHandleForReading];	
+	[task launch];
+	[task waitUntilExit];	
+	NSData *data;
+	data = [file readDataToEndOfFile];  
+	[task release];
+	NSString *splayer_uuid = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+  
+	NSString *filename = @"receipt";
+  NSString *boundary = @"----FOO";
+  
+  
+  NSURL *url = [NSURL URLWithString:@"https://www.shooter.cn/api/v2/auth.php"];
+  NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:url];
+  [req setHTTPMethod:@"POST"];
+  [req setValue:@"SPlayer 19780218 Mac OSX App" forHTTPHeaderField:@"User-Agent"];
+    
+  NSString *contentType = [NSString stringWithFormat:@"multipart/form- data, boundary=%@", boundary];
+  [req setValue:contentType forHTTPHeaderField:@"Content-type"];
+  
+  NSData *receiptData = [NSData dataWithContentsOfFile:receiptPath options:0 error:nil];
+  CFDataRef appstoreGuidRef = [self genAppstoreGuid];
+  NSData *appstoreGuid = [NSData dataWithBytes:CFDataGetBytePtr(appstoreGuidRef) length:CFDataGetLength(appstoreGuidRef)];
+
+  //adding the body:
+  NSMutableData *postBody = [NSMutableData data];
+  [postBody appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+  [postBody appendData:[@"Content-Disposition: form-data; name= \"splayer_uuid\"\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+  [postBody appendData:[splayer_uuid dataUsingEncoding:NSUTF8StringEncoding]];
+  [postBody appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+  [postBody appendData:[[NSString stringWithFormat:@"Content- Disposition: form-data; name=\"receipt_file\"; filename=\"%@\"\r\n", filename] dataUsingEncoding:NSUTF8StringEncoding]];
+  [postBody appendData:[@"Content-Type: binary/octet-stream\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+  [postBody appendData:receiptData];
+  [postBody appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+  [postBody appendData:[@"Content-Disposition: form-data; name= \"appstore_guid\"\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+  [postBody appendData:appstoreGuid];
+  [postBody appendData:[[NSString stringWithFormat:@"\r\n--%@--\r \n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+  [req setHTTPBody:postBody];
+  
+  [POOL release];
+}
 @end
