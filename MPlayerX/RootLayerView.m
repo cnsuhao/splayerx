a05bbdd363cfe92c418ec0d123716fa82c0b64b8
zhusichengroy <zhusichengroy@gmail.com>
2011-07-19 21:04:13 +0800

    save log and snapshot by correct app id(name)

--- MPlayerX/RootLayerView.m
+++ MPlayerX/RootLayerView.m
@@ -31,12 +31,17 @@
 #import "TitleView.h"
 #import "AppController.h"
 #import "GTMStringEncoding.h"
+#import "StoreHandler.h"
 
 #define kOnTopModeNormal		(0)
 #define kOnTopModeAlways		(1)
 #define kOnTopModePlaying		(2)
 
+#ifdef HAVE_STOREKIT
+#define kSnapshotSaveDefaultPath	(@"~/Pictures/SPlayerX Revised")
+#else
 #define kSnapshotSaveDefaultPath	(@"~/Pictures/SPlayerX")
+#endif
 
 @interface RootLayerView (RootLayerViewInternal)
 -(NSSize) calculateContentSize:(NSSize)refSize;
