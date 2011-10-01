5d045ba0600572f2f59b25fd326b23d15024b616
Tomasen <Tomasen@localhost>
2011-10-01 12:20:09 +0800

    adjust sub-projects structures to be able to build in Xcode 4

---
+++ SPlayerX/AppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.m
+//  SPlayerX
+//
+//  Created by  on 11-9-30.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
