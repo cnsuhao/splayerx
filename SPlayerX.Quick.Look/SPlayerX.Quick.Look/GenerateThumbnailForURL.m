085063d7dab37e323e54a1c02cc249bd2e3e3933
Tomasen <tomasen@gmail.com>
2014-10-25 16:05:35 +0800

    ??QuickLookPreview

--- SPlayerX.Quick.Look/SPlayerX.Quick.Look/GenerateThumbnailForURL.m
+++ SPlayerX.Quick.Look/SPlayerX.Quick.Look/GenerateThumbnailForURL.m
@@ -29,7 +29,6 @@ OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thum
 {
     // To complete your generator please implement the function GenerateThumbnailForURL in GenerateThumbnailForURL.c
     NSString* mpath = MPlayerPath();
-    NSLog(@"[SPQL] mplayer-mt: %@",  mpath);
     
     NSString *globallyUniqueString = [[NSProcessInfo processInfo] globallyUniqueString];
     
@@ -54,7 +53,7 @@ OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thum
     size_t thumb_height = CGImageGetHeight(thumb);
     
     // Preview will be drawn in a vectorized context
-    CGContextRef cgContext = QLThumbnailRequestCreateContext(thumbnail, CGSizeMake(thumb_width, thumb_height), false, NULL);
+    CGContextRef cgContext = QLThumbnailRequestCreateContext(thumbnail, CGSizeMake(thumb_width, thumb_height), true, NULL);
     if(cgContext) {
         CGContextDrawImage(cgContext, CGRectMake(0, 0, thumb_width, thumb_height), thumb);
         
@@ -73,3 +72,64 @@ void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbn
 {
     // Implement only if supported
 }
+
+
+OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
+void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
+
+/* -----------------------------------------------------------------------------
+ Generate a preview for file
+ 
+ This function's job is to create preview for designated file
+ ----------------------------------------------------------------------------- */
+
+OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
+{
+    // To complete your generator please implement the function GeneratePreviewForURL in GeneratePreviewForURL.c
+    // To complete your generator please implement the function GenerateThumbnailForURL in GenerateThumbnailForURL.c
+    NSString* mpath = MPlayerPath();
+    NSLog(@"[SPQL] mplayer-mt: %@",  mpath);
+    
+    NSString *globallyUniqueString = [[NSProcessInfo processInfo] globallyUniqueString];
+    
+    NSCharacterSet *notAllowedChars = [[NSCharacterSet characterSetWithCharactersInString:@"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"] invertedSet];
+    globallyUniqueString = [[globallyUniqueString componentsSeparatedByCharactersInSet:notAllowedChars] componentsJoinedByString:@""];
+    NSString *tmpdir = [NSTemporaryDirectory() stringByAppendingPathComponent:globallyUniqueString];
+    NSString *tmpfile = [tmpdir stringByAppendingPathComponent:@"00000001.png"];
+    NSString* moviepath = [(__bridge NSURL *)url path];
+    
+    // TODO: predict video length
+    NSArray *args = [NSArray arrayWithObjects:moviepath, @"-ss", @"5", @"-frames", @"1", @"-nosound", @"-vo",
+                     [NSString stringWithFormat:@"png:z=0:outdir=%@", tmpdir], nil];
+    [[NSTask launchedTaskWithLaunchPath:mpath arguments:args] waitUntilExit];
+    
+    CGDataProviderRef imgDataProvider = CGDataProviderCreateWithCFData((CFDataRef)[NSData dataWithContentsOfFile:tmpfile]);
+    CGImageRef thumb = CGImageCreateWithPNGDataProvider(imgDataProvider, NULL, true, kCGRenderingIntentDefault);
+    
+    if (thumb == nil)
+        return noErr;
+    
+    size_t thumb_width = CGImageGetWidth(thumb);
+    size_t thumb_height = CGImageGetHeight(thumb);
+    
+    // Preview will be drawn in a vectorized context
+    CGContextRef cgContext = QLPreviewRequestCreateContext(preview, CGSizeMake(thumb_width, thumb_height), true, NULL);
+    if(cgContext) {
+        CGContextDrawImage(cgContext, CGRectMake(0, 0, thumb_width, thumb_height), thumb);
+        
+        QLPreviewRequestFlushContext(preview, cgContext);
+        CFRelease(cgContext);
+        CGImageRelease(thumb);
+    }
+    
+    [[NSFileManager defaultManager] removeItemAtPath:tmpfile error:nil];
+    [[NSFileManager defaultManager] removeItemAtPath:tmpdir error:nil];
+    
+    return noErr;
+}
+
+void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview)
+{
+    // Implement only if supported
+}
+
